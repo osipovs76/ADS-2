eb0345a4b8f83603e763f1e0b0306cf11dbf612d
Osipov Andrey(21-IST5) <94702769+osipovs76@users.noreply.github.com>
2022-03-06 20:56:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -46,7 +46,7 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
   double a = 0;
   for (uint16_t i = 1; i <= count; i++) {
-    a += pown(-1, (i - 1)) * calcItem(x, 2 * i - 1);
+    a += pown(-1, (i - 1)) * calcItem(x, 2 * i - 2);
 
 	}
   return a;
